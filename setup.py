e284dc3af733193a2185500f2a96ef345b9ee627
Kazuyuki Akitsu <kakitsu@ias.edu>
2025-08-07 12:48:06 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,8 @@ setup(
     description="Utility routines for analyzing N-body data",
     author="Kazuyuki Akitsu",
     url="https://github.com/kazakitsu/lss_utils",
-    packages=find_packages(),
+    package_dir={"": "lss_utils"},
+    packages=find_packages(where='lss_utils'),
     install_requires=[
         "numpy>=1.20",
     ],
